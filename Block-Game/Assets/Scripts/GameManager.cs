428f57eecdf410a8041f2f488de50633d2a6a9e0
KevinLe563 <55369024+KevinLe563@users.noreply.github.com>
2020-06-25 03:24:21 -0600

    Game Setup (#1)

---
+++ Block-Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour {
+
+    public float restartDelay = 2f;
+    public GameObject gameOverUI;
+    bool gameHasEnded = false;
+    public void GameOver () {
+        
+        if (gameHasEnded == false) {
+            gameHasEnded = true;
+            gameOverUI.SetActive(true);
+            Invoke("RestartGame", restartDelay);
+        }
+
+    }
+
+    void RestartGame () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ Block-Game/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 4000f;
+    public float sidewaysForce = 100f;
+    // Start is called before the first frame update
+
+    // FixedUpdate used to change the physics
+    void FixedUpdate() {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        } else if (Input.GetKey("a")) {
+            rb.AddForce(sidewaysForce * -1  * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f) {
+            FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+}
