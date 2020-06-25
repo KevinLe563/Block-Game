dad463980360c290435166bfd232d89de16f2f47
Kevin <k26le@uwaterloo.ca>
2020-06-25 03:22:05 -0600

    Game Setup

---
+++ Block-Game/Assets/Scripts/CountScore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class CountScore : MonoBehaviour {
+
+    public Transform player;
+    public Text scoreboard;
+    // Update is called once per frame
+    void Update()
+    {
+        scoreboard.text = (player.position.z/25).ToString("0");
+    }
+}
